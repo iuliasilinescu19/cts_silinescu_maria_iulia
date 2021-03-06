3e7a8aad1faa925ada3dc8566f436f0cefd4b7c4
iuliasilinescu19 <62059180+iuliasilinescu19@users.noreply.github.com>
2021-03-06 12:06:04 +0200

    AM SALVAT PROIECTUL INITIAL

---
+++ seminar2/src/ro/ase/cts/Program/Program.java
@@ -0,0 +1,23 @@
+package ro.ase.cts.Program;
+
+import ro.ase.cts.Clase.Angajat;
+import ro.ase.cts.Clase.Utils;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Angajat> listaAngajati;
+		try {
+			listaAngajati = Utils.readAngajati("seminar2/angajati.txt");
+			for(Angajat angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
